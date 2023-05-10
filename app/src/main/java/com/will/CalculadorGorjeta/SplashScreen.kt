7cbdbb863ca9b2c8e1fb8b1cea9059b465208571
William <william.progdata@gmail.com>
2023-05-10 17:49:24 -0300

    Corrigindo FadeIn do texto da Splash Screen

--- app/src/main/java/com/will/CalculadorGorjeta/SplashScreen.kt
+++ app/src/main/java/com/will/CalculadorGorjeta/SplashScreen.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
@@ -13,9 +14,21 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         val imageSplashScreen = findViewById<ImageView>(R.id.image_splash_screen)
+        val textCalculador = findViewById<TextView>(R.id.text_Calculador)
+        val textCalculador2 = findViewById<TextView>(R.id.text_Calculador_2)
+        val duration : Long = 1600
+
         val fadeInAnimation = ObjectAnimator.ofFloat(imageSplashScreen, "alpha",0f,1f)
-        fadeInAnimation.duration = 1600
+        fadeInAnimation.duration = duration
         fadeInAnimation.start()
+
+        val fadeInAnimationText1 = ObjectAnimator.ofFloat(textCalculador,"alpha",0f,1f)
+        fadeInAnimationText1.duration = duration
+        fadeInAnimationText1.start()
+
+        val fadeInAnimationText2 = ObjectAnimator.ofFloat(textCalculador2,"alpha",0f,1f)
+        fadeInAnimationText2.duration = duration
+        fadeInAnimationText2.start()
 
         Handler().postDelayed({
             mostrarMainActiity()
